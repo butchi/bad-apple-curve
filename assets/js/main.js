9f03bdfccea39e0c32d51ad1238e678e3bfa6f8c
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-10-09 17:09:02 +0900

    fix function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,19 @@ window.licker = window.licker || {};
 (function(ns) {
   "use strict";
   ns.movieData = {};
-  var $canvas = $('.svg-canvas .svg-canvas__main');
-  ns.$canvas = $canvas;
-
-  /*
-   *  配列の値の総和
-   */
-  function total(arr) {
-    var i;
-    var len = arr.length;
-    var ret = 0;
-    for(i=0; i<len; i++) {
-      ret += arr[i];
-    }
-    return ret;
-  }
 
   $.getJSON('assets/data/fourier_arr.json', function(data) {
     ns.movieData = data;
     ns.currentFrame = 0;
+
     $(function() {
+      var $canvas = $('.svg-canvas .svg-canvas__main');
+      ns.$canvas = $canvas; // TODO: do not use global variable
+
       var animationPlayer = new ns.AnimationPlayer();
       animationPlayer.play();
     });
+
   });
 
   $(function() {
