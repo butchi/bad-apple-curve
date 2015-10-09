84b19ec534ee6339855023f94aa5243ffe74d7bf
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-10-09 18:11:08 +0900

    event handling

--- assets/js/MoviePlayer.js
+++ assets/js/MoviePlayer.js
@@ -5,21 +5,50 @@ window.licker = window.licker || {};
     this.audioPlayer = audioPlayer;
 
     this.frameRate = 30;
+
+    this.isPause = true;
+
+    var _self = this;
+
+    audioPlayer.$elm.on('play', function() {
+      console.log('play');
+      _self.play();
+    });
+
+    audioPlayer.$elm.on('pause', function() {
+      console.log('pause');
+      _self.pause();
+    });
+
+    audioPlayer.$elm.on('seeking', function() {
+      console.log('seeking');
+
+      var frame = Math.floor(_self.audioPlayer.audioElm.currentTime * _self.frameRate);
+      _self.animationPlayer.drawFrame(frame);
+    });
   }
 
   MoviePlayer.prototype.play = function() {
     this.audioPlayer.play();
+    this.isPause = false;
 
     var _self = this;
     requestAnimationFrame(loop);
 
     function loop() {
-      var frame = Math.floor(_self.audioPlayer.audioElm.currentTime * _self.frameRate);
-      _self.animationPlayer.drawFrame(frame);
+      var frame;
+      if(!_self.isPause) {
+        frame = Math.floor(_self.audioPlayer.audioElm.currentTime * _self.frameRate);
+        _self.animationPlayer.drawFrame(frame);
 
-      requestAnimationFrame(loop);
+        requestAnimationFrame(loop);
+      }
     }
   };
+
+  MoviePlayer.prototype.pause = function() {
+    this.isPause = true;
+  }
 
   ns.MoviePlayer = MoviePlayer;
 }(window.licker));
