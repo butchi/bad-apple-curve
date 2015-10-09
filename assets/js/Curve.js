504042d704d79b6ada98c12608d97c7aaf35c43d
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-10-09 18:26:24 +0900

    add comments

--- assets/js/Curve.js
+++ assets/js/Curve.js
@@ -1,13 +1,16 @@
 window.licker = window.licker || {};
 (function(ns) {
   function Curve(components) {
+    // components: 級数展開の係数の配列
     this.func = function(axis) {
+      // axis:  x: 0, y: 1
       return function(t) {
-        var ret = components[axis][0];
+        var ret = components[axis][0]; // 定数（周波数0）
         var i;
-        var len = components[axis].length;
-        for(i=1; i<len; i++) {
+        var len = components[axis].length; // 級数
+        for(i = 1; i < len; i++) {
           var cmp = components[axis][i];
+          // cmp[0]: 係数, cmp[1]: 位相
           ret += cmp[0] * Math.sin(i * t + cmp[1]);
         }
         return ret;
@@ -18,7 +21,7 @@ window.licker = window.licker || {};
 
     this.vertexArr = [];
     var t;
-    for(t=0; t<2*Math.PI; t+=Math.PI/128) {
+    for(t = 0; t < 2 * Math.PI; t += Math.PI/128) {
       this.vertexArr.push([this.funcX(t), this.funcY(t)]);
     }
   }
