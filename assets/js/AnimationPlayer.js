90ff3c1f151d84ed62274b5c08b0926fd187e517
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-10-12 11:13:06 +0900

    fix space

--- assets/js/AnimationPlayer.js
+++ assets/js/AnimationPlayer.js
@@ -11,7 +11,7 @@ window.licker = window.licker || {};
     var c;
     if(curveArr) {
       ns.$canvas.children().remove();
-      for(c=0; c<curveArr.length; c++) {
+      for(c = 0; c < curveArr.length; c++) {
         var curve = new ns.Curve(curveArr[c], this.maxFreq);
         curve.draw();
       }
